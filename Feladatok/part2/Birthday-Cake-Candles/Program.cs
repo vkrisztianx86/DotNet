747341bd7e0badf6773129407e903813a1afb1ca
Krisztian Vereczki <vkrisztianx86@outlook.com>
2024-03-24 15:54:56 +0100

    added

---
+++ Feladatok/part2/Birthday-Cake-Candles/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Birthday_Cake_Candles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+            int result = birthdayCakeCandles(candles);
+
+            Console.WriteLine(result);
+
+            
+        }
+        public static int birthdayCakeCandles(List<int> candles)
+        {
+            int count = 0;
+            int max = int.MinValue;
+            foreach (int num in candles)
+            {
+                if (num > max)
+                {
+                    max = num;
+                    count = 1; // Reset count for a new max height
+                }
+                else if (num == max)
+                {
+                    count++; // Increment count if current candle has the same height as max
+                }
+            }
+            return count;
+        }
+
+
+    }
+}
+
+
+
+
+
