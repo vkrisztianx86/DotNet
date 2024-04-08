a4d3973b5db241e2d1b20b197b8466dbeb9d0a74
Krisztian Vereczki <vkrisztianx86@outlook.com>
2024-04-08 21:58:53 +0200

    added

---
+++ Feladatok/ConsoleApp2Sol/ConsoleApp1Proj/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Kérem a szöveget!");
+string s = Console.ReadLine();
+
+Console.WriteLine("Kérem a darabolási méretet!");
+int size;
+int counter = 0;    
+
+while(!int.TryParse(Console.ReadLine(), out size))
+{
+    Console.WriteLine("Számot kell írni!");
+}
+
+
+string[] parts; 
+
+if(s.Length % size == 0)
+{
+    parts = new string[s.Length / size];
+}
+else
+{
+    parts = new string[s.Length / size + 1];
+}
+
+while(s.Length > 0)
+{
+    if(s.Length <= size)
+    {
+        parts[counter] = s;
+        s = String.Empty;
+    }
+    else
+    {
+        parts[counter] = s.Substring(0, size);
+        s = s.Substring(size);
+    }
+    counter++;  
+}
+
---
+++ WEBUNI/7Darabolo/Darabolo/Program.cs
@@ -0,0 +1,63 @@
+﻿string s;
+int meret;
+string[] darabok;
+
+Bekeres(out s, out meret);
+darabok = Meretezes(s, meret);
+Darabolas(s, meret, darabok);
+Kiiras(darabok);
+
+static void Kiiras(string[] darabok)
+{
+    foreach (string darab in darabok)
+    {
+        Console.WriteLine(darab);
+    }
+}
+
+static void Bekeres(out string sp, out int meretp)
+{
+    Console.WriteLine("Kérem a szöveget!");
+    sp = Console.ReadLine();
+
+    Console.WriteLine("Mérete?");
+
+    while (!int.TryParse(Console.ReadLine(), out meretp))
+    {
+        Console.WriteLine("Nem jó a méret");
+    }
+}
+
+static string[] Meretezes(string s, int meret)
+{
+    string[] darabok2;
+    if (s.Length % meret == 0)
+    {
+        darabok2 = new string[s.Length / meret];
+    }
+    else
+    {
+        darabok2 = new string[s.Length / meret + 1];
+    }
+    return darabok2;
+}
+
+static void Darabolas(string s, int meret, string[] dbk)
+{
+    int counter = 0;
+
+    while (s.Length > 0)
+    {
+        if (s.Length <= meret)
+        {
+            dbk[counter] = s;
+            s = String.Empty;
+        }
+        else
+        {
+            dbk[counter] = s.Substring(0, meret);
+            s = s.Substring(meret);
+        }
+        counter++;
+    }
+}
